297c5c57fae3381270389ae287743873bce0e07f
Wiz-Works <wizworks100@proton.me>
2025-10-04 23:00:06 -0400

    Update learnpeas.sh

--- learnpeas.sh
+++ learnpeas.sh
@@ -6589,7 +6589,7 @@ EOF
     echo -e "\033[32mMMMMMMMMMMMM\033[0m                                     \033[34mMMMMMMMMMMMM\033[0m                            "
     cat << "EOF"
     
-              Educational Privilege Escalation Tool - Version 1.5.0
+              Educational Privilege Escalation Tool - Version 1.7.0
     
     ════════════════════════════════════════════════════════════════
 
